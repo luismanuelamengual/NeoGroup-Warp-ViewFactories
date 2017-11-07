c4e155a2329b2ad02dc9ef858c474023f37c1074
Luis Amengual <luis.amengual@sitrack.com>
2017-11-07 15:15:30 -0300

    Cambios menores

--- NeoGroup-Warp-ViewFactory-Velocity/src/test/java/example/users/UsersController.java
+++ NeoGroup-Warp-ViewFactory-Velocity/src/test/java/example/users/UsersController.java
@@ -5,6 +5,8 @@ import org.neogroup.warp.controllers.routing.Get;
 import org.neogroup.warp.controllers.routing.Route;
 import org.neogroup.warp.views.View;
 
+import java.util.Map;
+
 import static org.neogroup.warp.Warp.createView;
 import static org.neogroup.warp.Warp.retrieveModel;
 import static org.neogroup.warp.Warp.retrieveModels;
@@ -14,9 +16,12 @@ public class UsersController {
 
     @Get("users")
     protected Route showUsers = (req, res) -> {
-        View usersView = createView("usersList");
+        View usersView = createView("usersList", Map.of("rama", "pepech"));
         usersView.setParameter("users", retrieveModels(User.class));
         usersView.setParameter("user", retrieveModel(User.class, 1));
         return usersView;
     };
+
+    @Get("test")
+    protected Route test = (req, res) -> "ramaaaa !!!";
 }
